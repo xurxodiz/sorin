f40db3475e82882b5fcd4ed2ed4afecabc7084e9
Jorge Diz <jorge@diz.es>
2013-04-16 23:38:03 +0200

    Prevent to generate [sic] tweets
    Checks if tweet exists in log, generates another in that case.

--- scripts/generate.py
+++ scripts/generate.py
@@ -8,34 +8,39 @@ import json
 with open("archive/"+sys.argv[1]+"/json", 'r') as f:
   chain = json.load(f)
 
-curr = "\n"
-numchars = -1 # offset the extra space counted in the first word
-sentence = []
-banned = [[]]
-while True:
-  choices = [x for x in chain[curr] if x not in banned[-1]]
-  if [] == choices:
-    #if len(banned) > 1:
-    banned.pop() 
-    #if len(sentence) > 0:
-    previous = sentence.pop()
-    banned[-1].append(curr)
-    numchars = numchars - 1 - len(curr)
-    curr = previous
-    continue
-  word = random.choice(choices)
-  if not word:
-    break # end of sentence
-  else:
-    if (numchars + 1 + len(word)) >= 140:
-      banned[-1].append(word)
-      continue
-    else:
-      numchars += 1 + len(word)
-      sentence.append(word)
-      banned.append([])
-      curr = word
+with open("archive/"+sys.argv[1]+"/json", 'r') as f:
+  tweets = [l.split() for l in f.readlines()]
 
-output = " ".join(sentence)
+output = tweets[0] # hack to guarantee a first iteration
+while output in tweets:
+
+  curr = "\n"
+  numchars = -1 # offset the extra space counted in the first word
+  sentence = []
+  banned = [[]]
+  while True:
+    choices = [x for x in chain[curr] if x not in banned[-1]]
+    if [] == choices:
+      #if len(banned) > 1:
+      banned.pop() 
+      #if len(sentence) > 0:
+      previous = sentence.pop()
+      banned[-1].append(curr)
+      numchars = numchars - 1 - len(curr)
+      curr = previous
+      continue
+    word = random.choice(choices)
+    if not word:
+      break # end of sentence
+    else:
+      if (numchars + 1 + len(word)) >= 140:
+        banned[-1].append(word)
+        continue
+      else:
+        numchars += 1 + len(word)
+        sentence.append(word)
+        banned.append([])
+        curr = word
+  output = " ".join(sentence)
 
 print(output.encode("utf-8"))
