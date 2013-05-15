c007ac0f074e4dcab2d3868f3ba768cd44a36421
Jorge Diz <jorge@diz.es>
2013-05-16 00:04:16 +0200

    Fix typo in 'sic' tweet check

--- scripts/generate.py
+++ scripts/generate.py
@@ -8,7 +8,7 @@ import json
 with open("archive/"+sys.argv[1]+"/json", 'r') as f:
   chain = json.load(f)
 
-with open("archive/"+sys.argv[1]+"/json", 'r') as f:
+with open("archive/"+sys.argv[1]+"/log", 'r') as f:
   tweets = [l.split() for l in f.readlines()]
 
 output = tweets[0] # hack to guarantee a first iteration
