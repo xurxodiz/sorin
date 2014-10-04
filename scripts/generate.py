ded642f720b82868e5e84e38912500f84f99f6bc
Jorge Diz <jorge@diz.es>
2014-10-04 14:50:29 +0200

    Simplify backtracking to backjumping

--- scripts/generate.py
+++ scripts/generate.py
@@ -5,6 +5,7 @@ import sys
 import random
 import json
 import xml.sax.saxutils
+import random
 
 with open("archive/"+sys.argv[1]+"/json", 'r') as f:
   chain = json.load(f)
@@ -12,35 +13,39 @@ with open("archive/"+sys.argv[1]+"/json", 'r') as f:
 with open("archive/"+sys.argv[1]+"/log", 'r') as f:
   tweets = [l.strip() for l in f.readlines()]
 
+
+####
+# jump back a random distance
+def backjump():
+  global sentence, numchars, curr
+  cutpoint = random.randint(1, len(sentence))
+  for i in range(1, cutpoint):
+    popped = sentence.pop()
+    numchars = numchars - 1 - len(popped)
+  curr = sentence[-1]
+####
+
 output = tweets[0] # hack to guarantee a first iteration
 while output in tweets:
 
   curr = "\n"
   numchars = -1 # offset the extra space counted in the first word
   sentence = []
-  banned = [[]]
   while True:
-    choices = [x for x in chain[curr] if x not in banned[-1]]
+    choices = [x for x in chain[curr]]
     if [] == choices:
-      if len(banned) > 1:
-        banned.pop() 
-      if len(sentence) > 0:
-        previous = sentence.pop()
-      banned[-1].append(curr)
-      numchars = numchars - 1 - len(curr)
-      curr = previous
+      backjump()
       continue
     word = random.choice(choices)
     if not word:
       break # end of sentence
     else:
       if (numchars + 1 + len(word)) >= 140:
-        banned[-1].append(word)
+        backjump()
         continue
       else:
         numchars += 1 + len(word)
         sentence.append(word)
-        banned.append([])
         curr = word
   output = " ".join(sentence)
 
