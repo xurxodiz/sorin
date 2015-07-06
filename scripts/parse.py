d327a354cd40dfd2a90cf4794a4aec0765066f5e
Jorge Diz <jorge@diz.es>
2015-07-06 14:23:13 +0200

    Add 2-gram dict parsing

--- scripts/parse.py
+++ scripts/parse.py
@@ -6,14 +6,22 @@ import sys
 
 with open("archive/"+sys.argv[1]+"/log") as f:
   chain = {}
+  chain2 = {}
 
   for tweet in f:
     words = tweet.strip().split()
+    prev = u"\n"
     curr = u"\n"
     for word in words:
       chain.setdefault(curr,[]).append(word)
+      chain2.setdefault(prev+" "+curr,[]).append(word)
+      prev = curr
       curr = word
     chain.setdefault(curr,[]).append(False)
+    chain2.setdefault(prev+" "+curr,[]).append(False)
 
 with open("archive/"+sys.argv[1]+"/json", 'w') as f:
   json.dump(chain, f, indent=2, sort_keys=True, ensure_ascii=False)
+
+with open("archive/"+sys.argv[1]+"/json2", 'w') as f:
+  json.dump(chain2, f, indent=2, sort_keys=True, ensure_ascii=False)
